951f914eb101658fcf81adeff101d36defab4486
Zhanbolat Seitkulov <zhanbolatseitkulov@gmail.com>
2019-09-30 17:02:10 +0600

    registering eureka client

--- src/main/java/kz/iitu/jd3/bookinfo/BookInfoApplication.java
+++ src/main/java/kz/iitu/jd3/bookinfo/BookInfoApplication.java
@@ -2,8 +2,10 @@ package kz.iitu.jd3.bookinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookInfoApplication {
 
 	public static void main(String[] args) {
